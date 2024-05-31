5f070c0c7d883eb179cb12915a6bbd7c2135e5c4
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-05-31 23:03:45 +0200

    add cors policy

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -11,6 +11,17 @@ class ProjectController extends Controller
     public function index()
     {
         $projects = Project::with('type', 'technologies')->orderByDesc('id')->paginate(6);
+
+        return response()->json([
+            'success' => true,
+            'projects' => $projects
+        ]);
+    }
+
+    public function featured()
+    {
+        $ids = [41, 40, 39, 38, 37, 31];
+        $projects = Project::with('type', 'technologies')->whereIn('id', $ids)->get();
         return response()->json([
             'success' => true,
             'projects' => $projects
